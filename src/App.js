8e38701b39ff9cd3e76cb403d35385371c2d8021
Greg <gregory_jego@hotmail.com>
2019-11-08 13:43:00 +0100

    avec recherch

--- src/App.js
+++ src/App.js
@@ -4,26 +4,50 @@ import "./App.css";
 function App() {
   const [newTask, setNewTask] = useState("");
   const [taskArray, setArray] = useState([]);
+  const [taskArrayDone, setArrayDone] = useState([]);
+  const [taskToFind, setTaskToFind] = useState("");
+  const [results, setResults] = useState("");
+
+  const deleteFromArray = task => {
+    for (let i = 0; i < taskArray.length; i++)
+      if (taskArray[i] === task) taskArray.splice(i, 1);
+    setArray(taskArray);
+  };
+
+  const addToArrayDone = task => {
+    setArrayDone([...taskArrayDone, task]);
+  };
 
   const list = taskArray.map(task => (
     <div
       className="task"
       key={task}
+      style={{ color: "grey" }}
       onClick={event => {
-        event.target.style.textDecoration = "line-through";
+        // event.target.style.textDecoration = "line-through";
+        deleteFromArray(task);
+        addToArrayDone(task);
       }}
     >
       X {task}
     </div>
   ));
+
+  const listDone = taskArrayDone.map(task => (
+    <div className="task" key={task} style={{ textDecoration: "line-through" }}>
+      X {task}
+    </div>
+  ));
+
   return (
     <div className="container">
       <h1>To-Do list</h1>
       {list}
+      {listDone}
       <input
         name="input-task"
         type="text"
-        placeholder="Titre"
+        placeholder="Titre de la tâche à ajouter"
         value={newTask}
         onChange={event => {
           setNewTask(event.target.value);
@@ -32,11 +56,37 @@ function App() {
       <br />
       <button
         onClick={() => {
-          setArray([...taskArray, newTask]);
-          setNewTask("");
+          if (newTask !== "") {
+            setArray([...taskArray, newTask]);
+            setNewTask("");
+          }
         }}
       >
         Ajouter une tâche
+      </button>
+      <hr />
+      <input
+        type="text"
+        placeholder="Titre de la tâche à trouver"
+        onChange={event => {
+          setTaskToFind(event.target.value);
+          setResults("");
+        }}
+      />
+      <span>{results}</span>
+      <button
+        onClick={() => {
+          if (taskArray.length === 0) setResults("Not found !");
+          else {
+            for (let i = 0; i < taskArray.length; i++)
+              if (taskArray[i] === taskToFind)
+                setResults("Found : " + taskArray[i]);
+              else setResults("Not found !");
+            // list[Object.keys(list)[i]].props.style.backgroundColor = "red";
+          }
+        }}
+      >
+        Trouver une tâche
       </button>
     </div>
   );
