05a6700ea996cc4722c20afce1feaf80c5526ae7
Greg <gregory_jego@hotmail.com>
2019-11-10 21:11:39 +0100

    maj

--- src/App.js
+++ src/App.js
@@ -69,7 +69,17 @@ function App() {
 
   return (
     <div className="container">
-      <h1>TRUCS À FAIRE</h1>
+      <h1>
+        TRUCS À FAIRE{" "}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="currentColor"
+          width="40"
+          viewBox="0 0 512 512"
+        >
+          <path d="M179.2 410.4L101.9 333 374.4 60.1l77.3 77.4-272.5 272.9zm-96-38.3l56.9 57-79.2 22.3 22.3-79.3zm411.8-249L388.8 16.7c-7.7-7.7-21.2-7.7-28.9 0L58.6 318.5c-2.5 2.5-4.3 5.5-5.2 8.9l-41.6 148c-2 7.1 0 14.8 5.2 20 3.9 3.9 11.7 6.7 20 5.2l147.8-41.7c3.4-.9 6.4-2.7 8.9-5.2L495 152c8-8 8-20.9 0-28.9z"></path>
+        </svg>
+      </h1>
       <div className="body">
         <ul>{list}</ul>
         <form
@@ -90,7 +100,7 @@ function App() {
         >
           <input
             type="text"
-            placeholder="Nouvelle tâche"
+            placeholder="Nouveau truc"
             value={taskInput}
             maxlength="50"
             onChange={event => {
